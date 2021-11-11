fc0c7875e1fbe9008a08ec4ba9e8e78acb3ce086
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-11 12:04:39 -0500

    1

--- calculate_drift.py
+++ calculate_drift.py
@@ -20,11 +20,15 @@ def calculate_drift(open_path, save_path):
             print(folder)
             try:
                 coordinates = pd.read_csv(root + '/' + folder + '/' + 'rpi-coordinates.csv')
+                drift = []
                 for index, row in coordinates.iterrows():
                   # print(row['rpi_lat'], row['rpi_lon'], row['gps_lat'], row['gps_lon'])
                   coords_1 = (row['rpi_lat'], row['rpi_lon'])
                   coords_2 = (row['gps_lat'], row['gps_lon'])
-                  print(geodesic(coords_1, coords_2).km * 1000, 'meters')
+                  # print(geodesic(coords_1, coords_2).km * 1000, 'meters')
+                  drift.append(geodesic(coords_1, coords_2).km * 1000)
+                coordinates['drift'] = drift
+                coordinates.to_csv(root + '/' + folder + '/' + 'rpi-coordinates-analyzed.csv', index=False)
             except:
                 print(sys.exc_info(), root + '/' + folder + '/')
 
