55757d6b10b0ead96976809839f6c8d50d0c4845
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-11 12:27:08 -0500

    1

--- calculate_drift.py
+++ calculate_drift.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import sys
 from geopy.distance import geodesic
+from geographiclib.geodesic import Geodesic
 
 def import_csv_as_df(csv_file):
   """
@@ -9,6 +10,10 @@ def import_csv_as_df(csv_file):
   """
   df = pd.read_csv(csv_file)
   return df
+
+def get_bearing(lat1, lat2, lon1, lon2):
+    brng = Geodesic.WGS84.Inverse(lat1, lon1, lat2, lon2)['azi1']
+    return brng
 
 def calculate_drift(open_path, save_path):
     """
@@ -21,13 +26,20 @@ def calculate_drift(open_path, save_path):
             try:
                 coordinates = pd.read_csv(root + '/' + folder + '/' + 'rpi-coordinates.csv')
                 drift = []
+                gps_heading = []
+                prev_gps_lat = coordinates['gps_lat'].iloc[0]
+                prev_gps_lon = coordinates['gps_lon'].iloc[0]
                 for index, row in coordinates.iterrows():
                   # print(row['rpi_lat'], row['rpi_lon'], row['gps_lat'], row['gps_lon'])
                   coords_1 = (row['rpi_lat'], row['rpi_lon'])
                   coords_2 = (row['gps_lat'], row['gps_lon'])
                   # print(geodesic(coords_1, coords_2).km * 1000, 'meters')
                   drift.append(geodesic(coords_1, coords_2).km * 1000)
-                coordinates['drift'] = drift
+                  gps_heading.append(get_bearing(prev_gps_lat, row['gps_lat'], prev_gps_lon, row['gps_lon']))
+                  prev_gps_lon = row['gps_lon']
+                  prev_gps_lat = row['gps_lat']
+                coordinates['drift_between_rpi_and_gps'] = drift
+                coordinates['gps_heading'] = gps_heading
                 coordinates.to_csv(root + '/' + folder + '/' + 'rpi-coordinates-analyzed.csv', index=False)
             except:
                 print(sys.exc_info(), root + '/' + folder + '/')
