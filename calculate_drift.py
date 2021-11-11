f98b058b123cfbcf1245489e3ed703c2381f76b3
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-11 13:04:06 -0500

    1

--- calculate_drift.py
+++ calculate_drift.py
@@ -3,6 +3,8 @@ import pandas as pd
 import sys
 from geopy.distance import geodesic
 from geographiclib.geodesic import Geodesic
+from turfpy import measurement
+from geojson import Point, Feature
 
 def import_csv_as_df(csv_file):
   """
@@ -12,8 +14,14 @@ def import_csv_as_df(csv_file):
   return df
 
 def get_bearing(lat1, lat2, lon1, lon2):
-    brng = Geodesic.WGS84.Inverse(lat1, lon1, lat2, lon2)['azi2']
-    return brng
+  brng = Geodesic.WGS84.Inverse(lat1, lon1, lat2, lon2)['azi2']
+  return brng
+  
+def get_turf_bearing(lat1, lat2, lon1, lon2):
+  start = Feature(geometry=Point((lon1, lat1)))
+  end = Feature(geometry=Point((lon2, lat2)))
+  brng = measurement.bearing(start,end)
+  return brng
 
 def calculate_drift(open_path, save_path):
     """
@@ -35,7 +43,7 @@ def calculate_drift(open_path, save_path):
                   coords_2 = (row['gps_lat'], row['gps_lon'])
                   # print(geodesic(coords_1, coords_2).km * 1000, 'meters')
                   drift.append(geodesic(coords_1, coords_2).km * 1000)
-                  gps_heading.append(get_bearing(prev_gps_lat, row['gps_lat'], prev_gps_lon, row['gps_lon']))
+                  gps_heading.append(get_turf_bearing(prev_gps_lat, row['gps_lat'], prev_gps_lon, row['gps_lon']))
                   prev_gps_lon = row['gps_lon']
                   prev_gps_lat = row['gps_lat']
                 coordinates['drift_between_rpi_and_gps'] = drift
