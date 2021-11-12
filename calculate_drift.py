f5b2079809c90908702571ef313ee3d47104c38e
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-12 12:36:21 -0500

    1

--- calculate_drift.py
+++ calculate_drift.py
@@ -87,7 +87,7 @@ def calculate_drift(open_path, save_path):
           max_end_row_range = len(coordinates)
           
           compass_vehicle_alignment_error = -2.5
-          doppler_compensation_factor = 1.012
+          doppler_compensation_factor = 1.017
           
           drift = []
           gps_minus_rpi_bearing_difference = []
