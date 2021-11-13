5def5322d589605fecc3dba581bcd1f3f220f112
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-13 12:30:52 -0500

    1

--- calculate_drift.py
+++ calculate_drift.py
@@ -25,7 +25,7 @@ def plot_data_in_plotly_bar_chart(df, save_path):
   """
   print('plotting charts')
   try:
-    chart = px.area(df, x=df.index, y=df.columns['rpi_bearing'], title=df.columns['rpi_bearing'], height=400)
+    chart = px.area(df, x=df.index, y=df.columns['rpi_bearing'], title='rpi_bearing', height=400)
     chart.update_layout(title_font_color="red", title_x=0.5, title_font_size=18)
     chart.write_html(save_path)
   except Exception as e:
